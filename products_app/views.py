6abd24bd2c8e33c77ea326c088985a9487c839d2
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2020-05-01 15:03:03 +0545

    Class based views changed to function

--- products_app/views.py
+++ products_app/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.views.generic import TemplateView
 
-
-class Index(TemplateView):
-    template_name = "products_app/index.html"
+def index(request):
+    return render(request, 'products_app/index.html')
