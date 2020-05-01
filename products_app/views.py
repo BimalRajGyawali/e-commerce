5c07611dfee4ee8d3960ca2b4b92f99a6b2711ee
dikshantGit <rams4582@gmail.com>
2020-05-01 14:00:43 +0545

    Index View

--- products_app/views.py
+++ products_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class Index(TemplateView):
+    template_name = "products_app/index.html"
