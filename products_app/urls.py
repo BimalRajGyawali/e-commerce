421a1b23caed6ed75390475417206ee7aabaac3e
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2020-05-03 14:10:21 +0545

    cart_save view adjusted

--- products_app/urls.py
+++ products_app/urls.py
@@ -4,10 +4,8 @@ from products_app.views import index, cart, checkout, cart_save
 
 urlpatterns = [path('', index, name='index'),
                path('cart/', cart, name='cart'),
-               path('cart-save/', cart_save, name='cart_save'),
+               path('cart-save/<int:id>', cart_save, name='cart_save'),
                path('checkout/', checkout, name='checkout'),
-
-
 ]
 
 
--- products_app/views.py
+++ products_app/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect
+from .models import Product, OrderItem, Item
 
 
 def index(request):
-    products = Product.objects.all();
+    products = Product.objects.all()[:2]
     return render(request, 'products_app/index.html', {'products': products})
 
 
 def cart(request):
-    return render(request, 'products_app/cart.html')
+    ordered_items = OrderItem.objects.all()
+    return render(request, 'products_app/cart.html', {'ordered_items': ordered_items})
 
 
 def checkout(request):
     return render(request, 'products_app/checkout.html')
 
 
-def cart_save(request):
-    return render(request, 'products_app/cart.html')
-
+def cart_save(request, id):
+    item = Item.objects.filter(id=id).first()
+    quantity = request.POST.get('quantity')
+    order_item = OrderItem(item=item, quantity=quantity)
+    order_item.save()
+    return redirect('cart')
